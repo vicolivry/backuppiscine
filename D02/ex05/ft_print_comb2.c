30665b9dcd04ec6cae1e10702317024d68eb1a9f
Victor Olivry <volivry@z2r1p3.le-101.fr>
2017-09-08 10:53:31 +0200

    exo 5

--- D02/ex05/ft_print_comb2.c
+++ D02/ex05/ft_print_comb2.c
@@ -1,14 +1,15 @@
-i/* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: volivry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/07 20:23:51 by volivry           #+#    #+#             */
-/*   Updated: 2017/09/07 20:49:18 by volivry          ###   ########.fr       */
+/*   Created: 2017/09/08 08:55:14 by volivry           #+#    #+#             */
+/*   Updated: 2017/09/08 10:47:05 by volivry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c)
@@ -20,24 +21,54 @@ void ft_print_comb2(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
-
+	int y;
+	int z;
+	
 	i = 0;
-	j = 0; 
-	//k = 0 ;
-	//l = 1;
-	while(i <= 8)
+	j = 0;
+	y = 0;
+	z = 1;
+	while(i <= 9)
 	{
 		while(j <= 9)
 		{
-			if(i < j)
+			while(y <= 9)
 			{
-				ft_putchar('0' + i);
-				ft_putchar('0' + j);
-				ft_putchar(' ');
+				while(z <= 9)
+				{
+					if(i + j < y + z)
+					{
+						if(y >= i && z > j)
+						{
+							ft_putchar('0' + i);
+							ft_putchar('0' + j);
+							ft_putchar(' ');
+						}
+					}
+					if(i + j < y + z)
+					{
+						if(y >= i && z > j)
+						{
+							ft_putchar('0' + y);
+							ft_putchar('0' + z);
+						
+						if(i != 9 ||  j != 8)
+						{
+							ft_putchar(',');
+							ft_putchar(' ');
+						}
+						}
+					}
+					z++;
+				}
+				z = 0;
+				y++;
 			}
+			y = 0;
+			j++;	
 		}
+		j = 0;
+		i++;	
 	}
 }
 
